04eda52b79b373479b329e7f64bc4be63f92600f
António Matos <a.matos@campus.fct.unl.pt>
2018-04-07 17:09:30 +0100

    Changes

--- src/impl/storage/Datanode.java
+++ src/impl/storage/Datanode.java
@@ -1,10 +1,16 @@
 package impl.storage;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,37 +18,53 @@ import java.util.Map;
 import utils.Random;
 
 public class Datanode implements api.storage.Datanode {
-	
+
 
 	private static final int INITIAL_SIZE = 32;
 	private Map<String, byte[]> blocks = new HashMap<>(INITIAL_SIZE);
 	private String path;
-	
-	
+
+
 	public Datanode(String uri) {
 		path = uri + "datanode";
 	}
-	
+
 	@Override
 	public String createBlock(byte[] data) {
 		String id = Random.key64();
 		blocks.put( id, data);
-		String r = path + "/" + id;
-		return r;
-	}
+		
+			try {
+				FileOutputStream fos = new FileOutputStream(id) ;
+				fos.write(data);
+
+			} catch (Exception e1) {
+				System.out.println("Error_2");
+
+			}
+
+			String r = path + "/" + id;
+			return r;
+		}
+	
 
 	@Override
 	public void deleteBlock(String block) {
 		System.out.println("delete: " + block);
 		blocks.remove(block);
+		File file = new File(block);
+		file.delete();
 	}
 
 	@Override
 	public byte[] readBlock(String block) {
-		byte[] data =  blocks.get(block);
-		if( data != null )
-			return data;
-		else
-			throw new RuntimeException("NOT FOUND");
+		byte[] data = null;
+		try {
+			data = Files.readAllBytes(new File("block").toPath());
+		} catch (IOException e) {
+			System.out.println("Error reading");
+		}
+		return data;
+		
 	}
 }
